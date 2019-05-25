9ae89705e0ad7e547b501453edd22a909469b7f3
Khushal Agarwal <hrithik.agarwal87@gmail.com>
2019-05-25 14:48:06 +0530

    Create Ladderophilia.cpp

---
+++ Basics Of Input-Output/Ladderophilia.cpp
@@ -0,0 +1,19 @@
+//Problem Link :- https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/ladderophilia/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"*   *"<<endl;
+		cout<<"*   *"<<endl;
+		cout<<"*****"<<endl;
+	}
+	cout<<"*   *"<<endl;
+	cout<<"*   *"<<endl;
+	// your code goes here
+	return 0;
+}
